503b436aa6fde4d8dfc8f0301b968f99c1b86177
Vitor Barbosa <vitorbarbosa789@gmail.com>
2021-05-26 09:21:32 -0300

    first commit :tada:

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <infos></infos>
+</template>
+
+<script>
+
+import infos from './components/shared/info-header.vue'
+export default {
+
+    components: {
+      'infos': infos 
+    }
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Inter:wght@300;600&display=swap');
+
+* {
+  font-family: 'Inter', sans-serif;
+
+}
+body  {   
+  background-color: #051D3B;
+
+}
+</style>
---
+++ src/components/shared/info-header.vue
@@ -0,0 +1,77 @@
+<template>
+  <div>
+      <nav class="navbar">
+        <ul class="navbar_list">
+          <li class="navbar_itens"><a href=""><img src="/src/assets/logo.png" alt="Logo" id="logo"></a></li>
+          <li><input type="text" placeholder="Busque por algo" id="search"></li>
+          <li><div class="user">
+                <img src="/src/assets/profile.jpg" alt="" id="avatar">
+                <p id="username">Vitor</p>
+              </div></li>
+        </ul>
+      </nav>
+  </div>
+</template>
+
+<script>
+export default {
+    props: ['infos']
+
+}
+</script>
+
+<style>
+.navbar_list {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  
+}
+#logo {
+  width: 200px;
+}
+
+.navbar_list {
+  list-style-type: none;
+}
+
+#search {
+  width: 600px;
+  height: 56px;
+  border-radius: 8px;
+  background: rgba(255, 255, 255, 0.16);
+}
+
+#search::placeholder {
+  
+  padding: 2%;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 16px;
+  line-height: 24px;
+  color: #FFFFFF;
+  opacity: 0.64;
+}
+
+.user {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  
+  
+}
+#avatar {
+  width: 50px;
+  height:50px;
+  padding: 10%;
+  border-radius: 50px;
+}
+#username {
+  margin-right: 50px;
+  font-size: 18px;
+  line-height: 24px;
+  color: white;
+}
+</style>
