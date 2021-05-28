ed1ccda4934897fa8b9d473842804e2fbcc0106b
Vitor Barbosa <vitorbarbosa789@gmail.com>
2021-05-28 02:02:26 -0300

    Implrementando o conceito de SPA ao projeto com o VueRoutes :sparkles:

--- src/main.js
+++ src/main.js
@@ -5,6 +5,13 @@ import App from './App.vue'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { routes } from './routes';
+// Utiliza-se entre chaves pois a exportação não é default.
+
+import VueRouter from 'vue-router';
+
+
+Vue.use(VueRouter);
 
 library.add(fas)
 
@@ -12,9 +19,18 @@ Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
 
+const router = new VueRouter({
+  routes,
+  mode: 'history'
+});
 
+// É importante lembrar que no caso do Vue é possível usar o "mode:'history'"
+// Para remover o '#' da url. Irá funcionar corretamente pois estamos em 
+// localhost. Entretanto, lembre lembrar de configurar o servidor para em
+// toda e qualquer situação, retornar o index.html
 new Vue({
   el: '#app',
   components: { App },
+  router,
   template: '<App/>'
 })
---
+++ src/routes.js
@@ -0,0 +1,9 @@
+import home from './components/home/home.vue';
+import comunidade from './components/comunidade/comunidade.vue';
+
+export const routes = [
+
+    { path: '', component: home },
+    { path: '/comunidade', component: comunidade }
+
+  ];
