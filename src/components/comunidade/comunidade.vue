98f379704d964ffbae23fc65e7eac70e2e4c2ba4
Vitor Barbosa <vitorbarbosa789@gmail.com>
2021-05-28 01:59:28 -0300

    Adicionando a nova pag: comunidade :sparkles:

---
+++ src/components/comunidade/comunidade.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+      <infos></infos>
+      <sidebar></sidebar>
+  </div>
+</template>
+
+<script>
+import infos from '../shared/info-header.vue'
+import sidebar from '../shared/sidebar.vue'
+export default {
+    components: {
+    'infos': infos,
+    'sidebar': sidebar,
+    }
+}
+</script>
+
+<style>
+
+</style>
